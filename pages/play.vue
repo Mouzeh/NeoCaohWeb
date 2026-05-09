77fb6eb72a5ef772df73ee8fea1faab970f166e4
Mouzeh <75455060+Mouzeh@users.noreply.github.com>
2026-05-09 17:22:27 -0400

    update play url to play.neotcg.cl

--- pages/play.vue
+++ pages/play.vue
@@ -1,8 +1,5 @@
 <script setup>
-navigateTo(
-  "https://pub-18e8e0ba7c674041a71cd8b24675786e.r2.dev/NeoTcgMac.html",
-  { external: true },
-);
+navigateTo("https://play.neotcg.cl/NeoTcgMac.html", { external: true });
 </script>
 
 <template>
