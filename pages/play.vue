c2e875b96d98fd077383d70fab4ec4bb224ff0f0
Mouzeh <75455060+Mouzeh@users.noreply.github.com>
2026-05-09 16:16:16 -0400

    update play page with r2 url

--- pages/play.vue
+++ pages/play.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="game-container">
-    <iframe
-      src="/game/index.html"
-      width="100%"
-      height="100vh"
-      frameborder="0"
-      allowfullscreen
-    />
-  </div>
+  <iframe
+    src="https://pub-18e8e0ba7c674041a71cd8b24675786e.r2.dev/NeoTcgMac.html"
+    style="width: 100vw; height: 100vh; border: none; display: block"
+    allowfullscreen
+  />
 </template>
