01be9965719727535899fec91f2da7c24f15c786
Mouzeh <75455060+Mouzeh@users.noreply.github.com>
2026-05-09 16:09:19 -0400

    add play page for godot web

---
+++ pages/play.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="game-container">
+    <iframe
+      src="/game/index.html"
+      width="100%"
+      height="100vh"
+      frameborder="0"
+      allowfullscreen
+    />
+  </div>
+</template>
